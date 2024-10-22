9fb85e37797e9b101f254d1dfdca0b8f0cc68a47
Aditya Arakal <aditya.arakal@apptware.com>
2024-10-22 16:21:04 +0530

    updated the bg color of the navbar when in the chart page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,30 @@
-import Logo from '../assets/Logo.png'
+import classNames from 'classnames';
+import Logo from '../assets/Logo.png';
+import { useCallback } from 'react';
 
 const Navbar = () => {
+  const isChartPage = window.location.href.includes('project-estimation');
+
+  const getClassName = useCallback(() => {
+    const defaultClassName = 'navbar-wrapper';
+
+    /**
+     * when in chart page, the background of the navbar should be purple too
+     */
+    return classNames(defaultClassName, {
+      'purple-bg': isChartPage
+    });
+  }, [isChartPage]);
+
   return (
-    <div className="navbar-wrapper">
+    <div className={getClassName()}>
       <nav>
         <a href="/">
-        <img
-          src={Logo}
-         />
+          <img src={Logo} />
         </a>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

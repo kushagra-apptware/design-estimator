3b8edf9cc8e8a9c8bcb0605cbe2bb3e4120d2972
Aditya Arakal <aditya.arakal@apptware.com>
2024-10-25 12:48:13 +0530

    fixed dates in gantt chart timeline cell

--- src/utils/estimationPageUtils/modifyStandardData.ts
+++ src/utils/estimationPageUtils/modifyStandardData.ts
@@ -1,66 +1,15 @@
+import { ganttChartConstants } from '../../constants/ganttChartConstants';
 import { taskItemTypes } from '../constants';
+import { calculateEndDate } from './calculateEndDate';
 
-const weekDays = ['S', 'S', 'M', 'T', 'W', 'T', 'F'];
+export const weekDays = ['S', 'S', 'M', 'T', 'W', 'T', 'F'];
 
-const standardDataInHours = {
-  DiscoveryPlanning: 40,
-  InformationArchitectureWireframing: 46,
-  VisualDesignPrototyping: 36,
-  TestingHandoff: 24,
-  PostHandoffSupportOptimization: 24
-};
-
-const colors = {
-  DiscoveryPlanning: {
-    backgroundColor: '#BAE813',
-    color: 'white'
-  },
-  InformationArchitectureWireframing: {
-    backgroundColor: '#E47912',
-    color: 'white'
-  },
-  VisualDesignPrototyping: {
-    backgroundColor: '#729DE7',
-    color: 'white'
-  },
-  TestingHandoff: {
-    backgroundColor: '#1EB7AA',
-    color: 'white'
-  },
-  PostHandoffSupportOptimization: {
-    backgroundColor: '#FC3A74',
-    color: 'white'
-  }
-};
-
-export const domainWiseComplexityInPercentage = {
-  'Ecommerce': 0,
-  'Healthcare': 40,
-  'Fintech': 50,
-  'Lifestyle': -10,
-  'Entertainment': 0,
-  'Gaming': 0,
-  'Elearning': 0,
-  'Data Science': -30
-};
-
-export const stageWiseComplexityInHours = {
-  Concept: 0,
-  Planning: -36,
-  Enhancement: -40,
-  Revamp: -40
-};
-
-const calculateEndDate = (
-  startDate: number,
-  totalUnits: number,
-  unitsPerDay: number
-) => {
-  const daysRequired = totalUnits / unitsPerDay;
-  const endDate = startDate + daysRequired - 1;
-
-  return Math.ceil(endDate);
-};
+const {
+  domainWiseComplexityInPercentage,
+  stageWiseComplexityInHours,
+  standardDataInHours,
+  colors
+} = ganttChartConstants;
 
 export const modifyStandardData = (
   domains: (keyof typeof domainWiseComplexityInPercentage)[],
